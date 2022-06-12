a9e14f59d4ce9c649d177abd421420e529a4c93a
Grovre <50428844+Grovre@users.noreply.github.com>
2022-06-12 03:15:25 -0500

    emptied

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,12 +1,6 @@
-import github.grovre.ArraysSupport;
-
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {3, 1, 2, -5};
-        ArraysSupport.countingSort(arr);
-        System.out.println(Arrays.toString(arr));
+
     }
 }
